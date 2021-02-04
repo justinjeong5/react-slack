ccd4460e362525e6b75378c336955359a787996e
justinjeong <justin.jeong5@gmail.com>
2021-02-04 11:12:40 +0900

    initial commit

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Menu from '../components/Menu'
+import Main from '../components/Main'
+
+
+function Home() {
+  return (
+    <div>
+      <div><Menu /></div>
+      <div><Main /></div>
+    </div>
+  )
+}
+
+export default Home
