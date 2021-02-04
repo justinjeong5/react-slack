68cbef74ff1ffdde9a60149f1f02cdfee68cb793
justinjeong <justin.jeong5@gmail.com>
2021-02-04 14:16:55 +0900

    fix: remove styled-components

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
-      ctx.renderPage = () => originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-      });
+      ctx.renderPage = () => originalRenderPage({});
 
       const initialProps = await Document.getInitialProps(ctx);
       return {
@@ -18,12 +14,11 @@ export default class MyDocument extends Document {
         styles: (
           <>
             {initialProps.styles}
-            {sheet.getStyleElement()}
           </>
         ),
       };
     } finally {
-      sheet.seal();
+
     }
   }
 
