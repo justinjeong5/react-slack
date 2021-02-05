11a11671687048be061f64482b1bd323248ed8f2
justinjeong <justin.jeong5@gmail.com>
2021-02-05 19:36:39 +0900

    add(component): Sender
    message sender for chat

--- client/components/Main/Sender.js
+++ client/components/Main/Sender.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useCallback, useMemo } from 'react'
+import { Form, Input, Button, Space } from 'antd'
+import { useDispatch } from 'react-redux'
+
+import Uploader from './Sender/Uploader'
 
 function Sender() {
+
+  const dispatch = useDispatch();
+
+  const onFinish = useCallback((values) => {
+    console.log(values)
+  }, [])
+
   return (
     <div>
-      Sender
+      <Form
+        name="basic"
+        onFinish={onFinish}
+      >
+        <Form.Item name="content">
+          <Input.TextArea placeholder="메세지" />
+        </Form.Item>
+        <Form.Item>
+          <Space style={{ float: 'right' }}>
+            <Button type="primary" htmlType="submit" >
+              전송
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
