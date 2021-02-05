c9f21f8b9d8a6dbfdc8bec35a39fbdab89a34d36
justinjeong <justin.jeong5@gmail.com>
2021-02-05 19:36:18 +0900

    add(component): RoomInfo
    current chat room detailed information viewer

--- client/components/Main/RoomInfo.js
+++ client/components/Main/RoomInfo.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { Typography, Card, Image } from 'antd'
+
+const { Title } = Typography;
 
 function RoomInfo() {
+
+  const { currentRoom } = useSelector(state => state.room)
+
+  const rootDivStyle = useMemo(() => ({
+    width: '100%',
+    height: '100%',
+    minHeight: '150px',
+    padding: '1em',
+    marginBottom: '1em'
+  }), [])
+
   return (
-    <div>
-      RoomInfo
+    <div style={rootDivStyle}>
+      {currentRoom?.title &&
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }} >
+          <div>
+            <Title level={2}>{currentRoom.title}</Title>
+            <p>{currentRoom.description}</p>
+          </div>
+          <Card style={{ border: 'none' }}>
+            <Card.Meta
+              avatar={<Image
+                style={{ height: 50, width: 50, borderRadius: 25 }}
+                src={currentRoom.writer.image}
+              />}
+              title={currentRoom.writer.nickname}
+              description={currentRoom.writer.email}
+            />
+          </Card>
+        </div>
+      }
     </div>
   )
 }
