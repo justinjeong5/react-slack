65e50e57eb16832517ab6bc835277693ca96c270
justinjeong <justin.jeong5@gmail.com>
2021-02-05 19:34:08 +0900

    add(component): ChatRooms
    chat rooms list on side menu

--- client/components/Menu/ChatRooms.js
+++ client/components/Menu/ChatRooms.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid'
+import { Typography } from 'antd'
+import { MessageOutlined, PlusSquareOutlined } from '@ant-design/icons'
+
+import ModalForm from './ChatRooms/ModalForm';
+
+import { GET_CURRENT_ROOM, GET_CHATS_OF_ROOM, } from '../../reducers/types';
+
+const { Title } = Typography;
 
 function ChatRooms() {
+
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(state => state.user)
+  const { currentRoom, roomList } = useSelector(state => state.room)
+  const [showModal, setShowModal] = useState(false);
+  const handleShow = useCallback(() => {
+    setShowModal(prev => !prev)
+  }, [])
+
+  // useEffect(() => {
+  //   dispatch({
+  //     type: LOAD_ROOM_REQUEST
+  //   })
+  // }, [])
+
+  const handleCurrent = useCallback((room) => () => {
+    dispatch({
+      type: GET_CURRENT_ROOM,
+      data: { room }
+    })
+    dispatch({
+      type: GET_CHATS_OF_ROOM,
+      data: { roomId: room.id }
+    })
+  }, [])
+
+  const style = useCallback((room) => {
+    return room.id === currentRoom.id ? { backgroundColor: 'gray', borderRadius: 4 } : null;
+  }, [currentRoom])
+
+
   return (
     <div>
-      ChatRooms
-    </div>
+      <Title level={4} style={{ color: 'white' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            <MessageOutlined />{` CHANNELS [${roomList.length}]`}
+          </div>
+          <div>
+            {currentUser.uid && <PlusSquareOutlined onClick={handleShow} />}
+          </div>
+        </div>
+      </Title>
+      {roomList.map(room => (<div key={uuidv4()} onClick={handleCurrent(room)} style={{ padding: '5px 10px', marginTop: 5, ...style(room) }}>
+        {`# ${room.title}`}
+      </div>))}
+      <ModalForm showModal={showModal} handleShow={handleShow} />
+    </div >
   )
 }
 
---
+++ client/components/Menu/ChatRooms/ModalForm.js
@@ -0,0 +1,90 @@
+import React, { useCallback, useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types'
+import { Form, Modal, Button, Space, Input } from 'antd'
+import { FormOutlined, FileTextOutlined } from '@ant-design/icons'
+
+import { CREATE_ROOM_REQUEST } from '../../../reducers/types'
+
+function ModalForm({ showModal, handleShow }) {
+
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(state => state.user)
+
+  const onFinish = useCallback((values) => {
+    dispatch({
+      type: CREATE_ROOM_REQUEST,
+      data: {
+        title: values.title,
+        description: values.description,
+        writer: {
+          uid: currentUser.uid,
+          nickname: currentUser.nickname,
+          email: currentUser.email,
+          image: currentUser.image,
+        }
+      }
+    })
+  }, [currentUser]);
+
+  const formTitleRules = useMemo(() => ([
+    { required: true, message: '방 제목을 입력해주세요.' },
+    { minLen: 3, message: '방 제목은 3글자 이상으로 입력해주세요.' }
+  ]), [])
+  const formDescriptionRules = useMemo(() => ([
+    { required: true, message: '방 소개를 입력해주세요.' },
+  ]), [])
+
+
+  return (
+    <Modal
+      title={<span><FormOutlined /> 방 만들기</span>}
+      visible={showModal}
+      onCancel={handleShow}
+      width={800}
+      footer={[]}
+    >
+      <Form
+        name="basic"
+        onFinish={onFinish}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ width: '700px' }}
+      >
+        <Form.Item
+          label="제목"
+          name="title"
+          rules={formTitleRules}
+        >
+          <Input prefix={<FormOutlined />} placeholder="제목" />
+        </Form.Item>
+        <Form.Item
+          label="소개"
+          name="description"
+          rules={formDescriptionRules}
+        >
+          <Input prefix={<FileTextOutlined />} placeholder="소개" />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Space style={{ float: 'right' }}>
+            <Button onClick={handleShow}>
+              취소
+          </Button>
+            <Button type="primary" htmlType="submit" >
+              방만들기
+          </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+ModalForm.propTypes = {
+  showModal: PropTypes.bool.isRequired,
+  handleShow: PropTypes.func.isRequired,
+}
+
+export default ModalForm
