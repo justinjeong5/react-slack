ba263ce6842c432668c8fb19228f6133af519b9d
justinjeong <justin.jeong5@gmail.com>
2021-02-05 19:35:04 +0900

    add(component): Starred
    favorited chat rooms list on side menu

--- client/components/Menu/Starred.js
+++ client/components/Menu/Starred.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { v4 as uuidv4 } from 'uuid'
+import { Typography } from 'antd'
+import { StarOutlined } from '@ant-design/icons'
+
+import { GET_CURRENT_ROOM, GET_CHATS_OF_ROOM } from '../../reducers/types'
+
+const { Title } = Typography
 
 function Starred() {
+
+  const dispatch = useDispatch();
+  const { starList } = useSelector(state => state.star)
+  const { currentRoom } = useSelector(state => state.room)
+
+  const handleCurrent = useCallback((star) => () => {
+    dispatch({
+      type: GET_CURRENT_ROOM,
+      data: { room: star }
+    })
+    dispatch({
+      type: GET_CHATS_OF_ROOM,
+      data: { roomId: star.id }
+    })
+  }, [])
+
+  const style = useCallback((star) => {
+    return star.id === currentRoom.id ? { backgroundColor: 'gray', borderRadius: 4 } : null;
+  }, [currentRoom])
+
+
   return (
     <div>
-      Starred
+      <Title level={4} style={{ color: 'white' }}>
+        <div>
+          <StarOutlined />{` STARRED [${starList.length}]`}
+        </div>
+      </Title>
+      {starList.map(star => (<div key={uuidv4()} onClick={handleCurrent(star)} style={{ paddingLeft: 10, ...style(star) }}>
+        {`# ${star.title}`}
+      </div>))}
     </div>
   )
 }
