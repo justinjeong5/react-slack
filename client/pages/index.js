8e5d1951b588eec26d55055a6a980d744c59655a
justinjeong <justin.jeong5@gmail.com>
2021-02-09 13:28:19 +0900

    add: SSR for index page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { END } from 'redux-saga'
+import axios from 'axios'
+import wrapper from '../store/configureStore'
 
 import Menu from '../components/Menu'
 import Main from '../components/Main'
+
+import { AUTH_USER_REQUEST, LOAD_ROOMS_REQUEST, LOAD_DIRECTS_REQUEST, LOAD_CHATS_REQUEST, LOAD_STARS_REQUEST } from '../reducers/types'
 
 function Home() {
   return (
@@ -11,5 +16,32 @@ function Home() {
     </div>
   )
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  axios.defaults.headers.Cookie = '';
+  const cookie = context.req ? context.req.headers.cookie : '';
+  if (cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+
+  context.store.dispatch({
+    type: AUTH_USER_REQUEST
+  });
+  context.store.dispatch({
+    type: LOAD_ROOMS_REQUEST
+  })
+  context.store.dispatch({
+    type: LOAD_DIRECTS_REQUEST
+  })
+  context.store.dispatch({
+    type: LOAD_CHATS_REQUEST
+  })
+  context.store.dispatch({
+    type: LOAD_STARS_REQUEST
+  })
+
+  context.store.dispatch(END);
+  await context.store.sagaTask.toPromise();
+});
 
 export default Home
