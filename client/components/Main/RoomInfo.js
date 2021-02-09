32ab4a291a98df70f7bbce9dd51614f51acc91d2
justinjeong <justin.jeong5@gmail.com>
2021-02-09 13:31:58 +0900

    feat: room information header

--- client/components/Main/RoomInfo.js
+++ client/components/Main/RoomInfo.js
@@ -1,12 +1,45 @@
-import React, { useMemo } from 'react'
-import { useSelector } from 'react-redux'
-import { Typography, Card, Image } from 'antd'
+import React, { useCallback, useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Typography, Card, Image, message } from 'antd'
+import { StarOutlined, LockFilled, UnlockOutlined, StarFilled } from '@ant-design/icons'
+
+import { ADD_STAR_REQUEST, REMOVE_STAR_REQUEST } from '../../reducers/types'
 
 const { Title } = Typography;
 
 function RoomInfo() {
 
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(state => state.user)
   const { currentRoom } = useSelector(state => state.room)
+
+  const handleStar = useCallback(() => {
+    if (!currentUser._id) {
+      return message.warning('로그인이 필요한 서비스입니다.');
+    }
+    const data = {
+      roomId: currentRoom._id,
+      type: currentRoom.private ? 'private' : 'public'
+    }
+    dispatch({
+      type: ADD_STAR_REQUEST,
+      data
+    })
+  }, [currentRoom, currentUser])
+
+  const handleUnstar = useCallback(() => {
+    if (!currentUser._id) {
+      return message.warning('로그인이 필요한 서비스입니다.');
+    }
+    const data = {
+      roomId: currentRoom._id,
+      type: currentRoom.private ? 'private' : 'public'
+    }
+    dispatch({
+      type: REMOVE_STAR_REQUEST,
+      data
+    })
+  }, [currentRoom, currentUser])
 
   const rootDivStyle = useMemo(() => ({
     width: '100%',
@@ -36,6 +69,15 @@ function RoomInfo() {
           </Card>
         </div>
       }
+      <div style={{ float: 'right', marginRight: 30, fontSize: '1.5em' }}>
+        {currentRoom.starred?.includes(currentUser._id)
+          ? <StarFilled onClick={handleUnstar} />
+          : <StarOutlined onClick={handleStar} />}
+        {currentRoom.privates
+          ? <LockFilled />
+          : <UnlockOutlined />}
+
+      </div>
     </div>
   )
 }
