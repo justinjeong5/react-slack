7924f1f4893d86aee554331edd243ca8604c6f66
justinjeong <justin.jeong5@gmail.com>
2021-02-14 15:00:44 +0900

    add: next.config.js webpack setting

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,14 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production'
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+    }
+  },
+});
